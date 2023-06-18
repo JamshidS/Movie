e5e2f6e83b661b4031a0b4f45cadab0867bea998
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-06-18 23:39:10 +0300

    update user class

--- src/main/java/com/movie/movieservice/datta/User.java
+++ src/main/java/com/movie/movieservice/datta/User.java
@@ -1,12 +1,27 @@
 package com.movie.movieservice.datta;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String fullName;
+
+    @Column(name ="data_of_birth")
+    private String dateOfBirth;
+
 }
