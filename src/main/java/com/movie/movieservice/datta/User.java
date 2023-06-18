d448d4d61f9fdf156ec38674a30308e0f7dcb482
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-06-18 23:33:56 +0300

    create user class

---
+++ src/main/java/com/movie/movieservice/datta/User.java
@@ -0,0 +1,12 @@
+package com.movie.movieservice.datta;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    
+}
