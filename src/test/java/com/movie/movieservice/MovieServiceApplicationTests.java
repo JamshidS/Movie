5fcda65b5babf52ab01191fcc7c6a416a8392997
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-06-18 22:27:15 +0300

    initial commit

---
+++ src/test/java/com/movie/movieservice/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
