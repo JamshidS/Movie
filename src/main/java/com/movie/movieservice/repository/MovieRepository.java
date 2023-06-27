e213aefffdd88e2e231cb0caa238b2cd028468c8
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-06-27 13:44:56 +0300

    update servie of movie

--- src/main/java/com/movie/movieservice/repository/MovieRepository.java
+++ src/main/java/com/movie/movieservice/repository/MovieRepository.java
@@ -3,5 +3,5 @@ package com.movie.movieservice.repository;
 import com.movie.movieservice.data.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<Long, Movie> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
--- src/main/java/com/movie/movieservice/service/serviceImpl/MovieServiceImpl.java
+++ src/main/java/com/movie/movieservice/service/serviceImpl/MovieServiceImpl.java
@@ -1,7 +1,61 @@
 package com.movie.movieservice.service.serviceImpl;
 
+import com.movie.movieservice.bean.MovieResponse;
+import com.movie.movieservice.data.Movie;
+import com.movie.movieservice.dto.MovieDto;
+import com.movie.movieservice.repository.MovieCategoryRepository;
+import com.movie.movieservice.repository.MovieRepository;
+import com.movie.movieservice.service.MovieService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class MovieServiceImpl {
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository repository;
+    private final MovieCategoryRepository categoryRepository;
+
+    public MovieServiceImpl(MovieRepository repository, MovieCategoryRepository categoryRepository) {
+        this.repository = repository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public MovieResponse getMovieWithId(Long id) throws Exception {
+        Optional<Movie> movieOptional = repository.findById(id);
+        MovieResponse response = new MovieResponse();
+        List<String> list = new ArrayList<>();
+        if(!movieOptional.isPresent()){
+            throw new Exception("Movie not exists with this id: "+id);
+        }
+        response.setName(movieOptional.get().getName());
+        response.setDescription(movieOptional.get().getDescription());
+        movieOptional.get().getMovieCategoryList().forEach(x -> list.add(String.valueOf(x.getName())));
+
+        return response;
+    }
+
+    @Override
+    public List<MovieResponse> getAllMovies() {
+        return null;
+    }
+
+    @Override
+    public MovieResponse createMovie(MovieDto dto) {
+        return null;
+    }
+
+    @Override
+    public String updateMovie(MovieDto dto) {
+        return null;
+    }
+
+    @Override
+    public void deleteMovie(Long id) {
+
+    }
 }
