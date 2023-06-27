68db9914542e141aff7fedfeef7aab3aea427b1e
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-06-27 13:08:02 +0300

    create user preferences

---
+++ src/main/java/com/movie/movieservice/data/UserPreference.java
@@ -0,0 +1,27 @@
+package com.movie.movieservice.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_preferences")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPreference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "uuid")
+    private String uuid;
+
+
+
+}
